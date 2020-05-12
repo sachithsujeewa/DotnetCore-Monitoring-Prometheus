32d2f384f73c3f3b5add3de0ef7b8ab8c10ac277
sachith sujeewa <sachithsujeewa@live.com>
2020-05-12 09:32:37 +0530

    Initial commit with user crud api

---
+++ MonitoringPrometheusAndGrafana/MonitoringPrometheusAndGrafana/Data/MonitoringPrometheusAndGrafanaContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MonitoringPrometheusAndGrafana.Models;
+
+namespace MonitoringPrometheusAndGrafana.Data
+{
+    public class MonitoringPrometheusAndGrafanaContext : DbContext
+    {
+        public MonitoringPrometheusAndGrafanaContext (DbContextOptions<MonitoringPrometheusAndGrafanaContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<User> User { get; set; }
+    }
+}
